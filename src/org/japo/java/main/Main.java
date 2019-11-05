4478d9d1b9fe6a796ca38196babec14ef26b4590
AdrianDAW <Adriandaw@gmail.com>
2019-11-05 08:37:13 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,45 @@
+/* 
+ * Copyright 2019 Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>
+ */
+public class Main {
+    
+    public static final Random RND = new Random();
+
+    public static void main(String[] args) {
+        final int P_MIN = 1;
+        final int P_MAX = 39;
+        final int N_MARK = 6;
+        final int i = 0;
+        int n;
+//      Estado inicial
+        n = i;
+//      Inicio bucle
+        do {
+            n++;
+            int primitiva = RND.nextInt(P_MAX - P_MIN + 1) + P_MIN;
+            System.out.printf("Número %d ...: %d%n", n, primitiva);
+
+        } while (n < N_MARK);
+        System.out.println("END");
+    }
+}
